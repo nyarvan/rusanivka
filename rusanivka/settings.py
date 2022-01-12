675deb22965eaaed802164d363db407e7e3c655b
nyarvan <90792424+nyarvan@users.noreply.github.com>
2022-01-12 16:22:30 +0200

    Update settings.py

--- rusanivka/settings.py
+++ rusanivka/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIAFILES_DIRS = (MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

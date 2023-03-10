7cb41a0e8b59c8dfe6bd015a8dc92c8d21d1d3e6
Ivan <infinity.man102@gmail.com>
2023-03-10 14:58:16 +0200

    Edit admin settings for models on admin page

--- user_app/admin.py
+++ user_app/admin.py
@@ -4,51 +4,52 @@ from .models import Department, Doctor, Administration, Blog, BlogImage, Contact
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'full_name', 'number', 'manager', 'image', 'phone', 'address']
-    list_filter = ['number',]
-    list_editable = ['name', 'full_name', 'manager', 'image', 'phone', 'address']
-    list_display_links = ['number',]
+    list_display = ['name', 'manager', 'image']
+    list_filter = ['number', ]
+    list_editable = ['name', 'manager', 'image']
+    list_display_links = ['name', ]
 
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ['department', 'name', 'image', 'post', 'room', 'schedule', 'is_manager', 'is_visible']
+    list_display = ['name', 'image', 'post', 'department', 'is_manager', 'is_visible']
     list_filter = ['department', 'post', 'is_manager', 'is_visible']
-    list_editable = ['image', 'post', 'room', 'schedule', 'is_manager', 'is_visible']
+    list_editable = ['image', 'post', 'is_manager', 'is_visible']
+    list_display_links = ['name', ]
 
 
 @admin.register(Administration)
 class AdministrationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'position', 'image', 'post', 'schedule', 'phone']
-    list_filter = ['position',]
-    list_editable = ['position', 'image', 'post', 'schedule', 'phone']
+    list_display = ['name', 'image', 'post']
+    list_filter = ['position', ]
+    list_editable = ['image', 'post']
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'image', 'text', 'create']
-    list_filter = ['create',]
-    list_editable = ['image', 'text']
-    list_display_links = ['title']
+    list_display = ['title', 'image', 'create']
+    list_filter = ['create', ]
+    list_editable = ['image', ]
+    list_display_links = ['title', ]
     prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(BlogImage)
 class BlogImageAdmin(admin.ModelAdmin):
     list_display = ['blog', 'image']
-    list_filter = ['blog',]
-    list_editable = ['image',]
+    list_filter = ['blog', ]
+    list_editable = ['image', ]
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ['name', 'file']
-    list_filter = ['name',]
-    list_editable = ['file',]
+    list_filter = ['name', ]
+    list_editable = ['file', ]
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'subject', 'message', 'date', 'is_processing']
+    list_display = ['name', 'email', 'subject', 'date', 'is_processing']
     list_filter = ['date', 'is_processing']
-    list_editable = ['is_processing',]
+    list_editable = ['is_processing', ]

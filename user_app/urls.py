bc6d8f679ef27638881237bebede0469824b1f01
Ivan <infinity.man102@gmail.com>
2023-03-19 20:03:41 +0200

    Fix problem with return blog

--- user_app/urls.py
+++ user_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('administration/', AdministrationView.as_view(), name='admin_view'),
     path('ambulant<int:id>/', DepartmentView.as_view(), name='ambulant_view'),
     path('blog/news/', BlogsView.as_view(), name='blogs_view'),
-    path('blog/news/<slug:slug>/', BlogDetailView.as_view(), name='blog_single_view'),
+    path('blog/news/<int:id>/<slug:slug>/', BlogDetailView.as_view(), name='blog_single_view'),
     path('contact/', ContactView.as_view(), name='contact_view'),
     path('documents/statut/', document_view, name='document_view'),
 ]

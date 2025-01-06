df31dc7890bc242cbf9d7e60eceff131a37dd18b
Ivan Rybak <nyarvan@ivans-laptop.lan>
2025-01-06 13:59:29 -0500

    Fix problem with import

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import HomepageView, AdministrationView, DepartmentView, \
-                    BlogsView, BlogDetailView, ContactView, DrugsView, \
-                    MedicalServicesView, FreeMedicalServicesView
+                    BlogsView, BlogDetailView, ContactView, DrugsView
 
 app_name = 'user_app'
 

e896007d8222235c9387b8162f56099a4e67c1ab
Ivan Rybak <nyarvan@ivans-laptop.lan>
2025-01-06 13:54:45 -0500

    Delete templates, view classes and urls about medical services. Update urls of medical services in index.html

--- user_app/urls.py
+++ user_app/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path('ambulant<int:id>/', DepartmentView.as_view(), name='ambulant_view'),
     path('information-for-clients/medicines/',
          DrugsView.as_view(), name='drugs_view'),
-     path('information-for-clients/services/unpaid/', FreeMedicalServicesView.as_view(), name='free_medical_services'),
-     path('information-for-clients/services/paid/', MedicalServicesView.as_view(), name='medical_services'),
     path('blog/<slug:slug>/', BlogsView.as_view(), name='blogs_view'),
     path('blog/<slug:slug_category>/<int:id>/<slug:slug>/',
          BlogDetailView.as_view(), name='blog_single_view'),
